f873323fed3bf9db1e2a441c8b7bef5bbb2b97ce
laboratorio <laboratorio@UPOAULA10301>
2024-10-29 07:33:03 -0500

    Agregar archivos de proyecto.

---
+++ README.md
@@ -0,0 +1,1 @@
+# VelaAngel-ExamenProgreso
---
+++ VelaAngel-ExamenProgreso/Models/AVela.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace VelaAngel_ExamenProgreso.Models
+{
+    public class AVela
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public float Altura { get; set; }
+        public bool Hincha { get; set; }
+        public DateTime Nacimiento { get; set; }
+    }
+}
