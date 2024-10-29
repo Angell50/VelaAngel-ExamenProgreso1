6b13c855e15cd1a41944b3029a93354945241bbc
Angell50 <angel.vela@udla.edu.ec>
2024-10-29 07:41:37 -0500

    MODELO CELULAR

--- VelaAngel-ExamenProgreso/Models/AVela.cs
+++ VelaAngel-ExamenProgreso/Models/AVela.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace VelaAngel_ExamenProgreso.Models
 {
     public class AVela
     {
-
+        [Required]
         public int Id { get; set; }
         [MaxLength(100)]
         public string Nombre { get; set; }
+        [Range(0, 5)]
         public float Altura { get; set; }
+        [DisplayName("Hincha de LDU?")]
         public bool Hincha { get; set; }
         public DateTime Nacimiento { get; set; }
     }
---
+++ VelaAngel-ExamenProgreso/Models/Celular.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VelaAngel_ExamenProgreso.Models
+{
+    public class Celular
+    {
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Modelo { get; set; }
+        [Range(1900, 2024)]
+        public string Year { get; set; }
+
+        public float Precio { get; set; }
+    }
+}
