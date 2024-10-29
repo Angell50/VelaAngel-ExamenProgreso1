b752bef946592e217fc21bdbe032da6702ce5119
Angell50 <angel.vela@udla.edu.ec>
2024-10-29 07:35:27 -0500

    modelo AVela

--- VelaAngel-ExamenProgreso/Models/AVela.cs
+++ VelaAngel-ExamenProgreso/Models/AVela.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System.ComponentModel.DataAnnotations;
 
 namespace VelaAngel_ExamenProgreso.Models
 {
     public class AVela
     {
+
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Nombre { get; set; }
         public float Altura { get; set; }
         public bool Hincha { get; set; }
